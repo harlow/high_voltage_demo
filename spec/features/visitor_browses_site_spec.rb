2389daf45a1d4cab2a975c646c662febed83fda4
Harlow Ward <harlow@thoughtbot.com>
2013-12-06 12:07:20 -0800

    About Us page
    * Add new About Us page
    * Add a navigation partial

--- spec/features/visitor_browses_site_spec.rb
+++ spec/features/visitor_browses_site_spec.rb
@@ -6,4 +6,12 @@ feature 'Visitor browses site' do
 
     expect(page).to have_css('h1', 'High Voltage Demo')
   end
+
+  scenario 'vist the about us page' do
+    visit root_path
+
+    click_link 'About Us'
+
+    expect(page).to have_css('h1', 'About Us')
+  end
 end
